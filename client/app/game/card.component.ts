6da433a3437a562e57aecd90efd49fe26c43c928
Artemis-Haven <artemis.hacktivation@gmail.com>
2017-09-06 01:03:46 +0200

    Amélioration de la présentation d'un adversaire, à droite du plateau

--- client/app/game/card.component.ts
+++ client/app/game/card.component.ts
@@ -3,7 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
 	selector: 'card',
 	template: `
-	<div class="card" dnd-draggable [dragEnabled]="true" [dragData]="card" [attr.data-id]="card._id">
+	<div class="card card-{{size}}" dnd-draggable [dragEnabled]="true" [dragData]="card" [attr.data-id]="card._id">
 		<div class="card-name">{{card.name}}</div>
 		<div class="card-date">{{card.displayedDate}}</div>
 	</div>`,
@@ -12,6 +12,7 @@ import {Component, Input} from '@angular/core';
 
 export class CardComponent {
 	@Input() public card: any;
+	@Input() public size: String;
     receivedData: Array<any> = [];
 
     transferDataSuccess($event: any) {
@@ -19,6 +20,6 @@ export class CardComponent {
     }
 
 	constructor () {
-		
+		this.size = 'normal';
 	}
 }
--- client/app/game/opponent.component.ts
+++ client/app/game/opponent.component.ts
@@ -3,12 +3,14 @@ import {Component, Input} from '@angular/core';
 @Component({
 	selector: 'opponent',
 	template: `
-	<h3>{{user.username}}</h3>
-  	<ng-container *ngFor="let card of cards">
-		<ng-container *ngIf="card.user == user._id">
-			<card [card]=card></card>
+	<h4>{{user.username}}</h4>
+	<div class="opponentCards">
+	  	<ng-container *ngFor="let card of cards">
+			<ng-container *ngIf="card.user == user._id">
+				<card [card]=card [size]="'small'"></card>
+			</ng-container>
 		</ng-container>
-	</ng-container>`,
+	</div>`,
 	styleUrls: ['../game/game.component.scss'],
 })
 
